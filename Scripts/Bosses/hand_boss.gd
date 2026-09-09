b55d26a3cfa49ddfe4c05695024d34089dcef1da
PretendSushi <ayrtonottoni1@gmail.com>
2026-09-09 13:17:05 -0400

    Hand boss added. Animations handled

---
+++ Scripts/Bosses/hand_boss.gd
@@ -0,0 +1,108 @@
+extends CharacterBody2D
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+const MAX_HEALTH = 1000
+const MOVEMENT_SPEED = 500
+const ATTACK_TIMER = 5
+const ATTACK_ANIM_OFFSET = 250
+
+enum ActionState { IDLE, MOVING, ATTACK }
+enum AttackState { IDLE, START, DURATION, END }
+enum Directions { NONE, LEFT, RIGHT }
+
+var health
+
+var direction
+var action_state
+var attack_state
+
+var attack_timer
+
+var can_attack
+
+func _ready() -> void:
+	health = MAX_HEALTH
+	attack_timer = ATTACK_TIMER
+	action_state = ActionState.IDLE
+	attack_state = AttackState.IDLE
+	can_attack = true
+
+func _physics_process(delta: float) -> void:
+	var player_x = find_player_x()
+	if player_x:
+		move(player_x)
+	handle_attack_timer(delta)
+	attack()
+	play_animations()
+	move_and_slide()
+	
+func find_player_x():
+	var player = get_tree().get_first_node_in_group("Player")
+	if !player:
+		return
+	return player.global_position.x
+
+func move(player_x):
+	if action_state == ActionState.ATTACK:
+		return
+	if player_x + 10 > global_position.x and player_x - 10 > global_position.x:
+		velocity.x = MOVEMENT_SPEED
+		direction = Directions.RIGHT
+		action_state = ActionState.MOVING
+	elif player_x + 10 < global_position.x and player_x - 10 < global_position.x:
+		velocity.x = -MOVEMENT_SPEED
+		direction = Directions.LEFT
+		action_state = ActionState.MOVING
+	else:
+		velocity.x = 0
+		direction = Directions.NONE
+		action_state = ActionState.IDLE
+		
+func play_animations():
+	var target_anim = "idle"
+	if action_state == ActionState.MOVING:
+		if direction == Directions.RIGHT:
+			target_anim = "right"
+		elif direction == Directions.LEFT:
+			target_anim = "left"
+	elif action_state == ActionState.IDLE:
+		target_anim = "idle"
+	elif action_state == ActionState.ATTACK:
+		if attack_state == AttackState.START:
+			target_anim = "attack_start"
+		elif attack_state == AttackState.DURATION:
+			target_anim = "attack"
+		elif attack_state == AttackState.END:
+			target_anim = "attack_end"
+	
+	if target_anim == "attack_start" or target_anim == "attack" or target_anim == "attack_end":
+		animated_sprite.offset.y = ATTACK_ANIM_OFFSET
+	else:
+		animated_sprite.offset.y = 0
+	animated_sprite.play(target_anim)
+		
+
+func attack():
+	if action_state == ActionState.ATTACK or !can_attack:
+		return
+	action_state = ActionState.ATTACK
+	attack_state = AttackState.START
+	velocity.x = 0
+	can_attack = false
+	attack_timer = ATTACK_TIMER
+	
+func handle_attack_timer(delta):
+	if attack_timer > 0:
+		attack_timer -= delta
+	else:
+		can_attack = true
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite.animation == "attack_start":
+		attack_state = AttackState.DURATION
+	if animated_sprite.animation == "attack":
+		attack_state = AttackState.END
+	if animated_sprite.animation == "attack_end":
+		attack_state = AttackState.IDLE
+		action_state = ActionState.IDLE
