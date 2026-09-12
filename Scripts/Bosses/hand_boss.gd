a98c047f5f49ca586152b9fb0a91335306312461
PretendSushi <ayrtonottoni1@gmail.com>
2026-09-11 23:28:25 -0400

    Hand Boss new animations

--- Scripts/Bosses/hand_boss.gd
+++ Scripts/Bosses/hand_boss.gd
@@ -85,8 +85,8 @@ func play_animations():
 		elif attack_state == AttackState.END:
 			target_anim = "attack_end"
 	
-	if target_anim == "attack_start" or target_anim == "attack" or target_anim == "attack_end":
-		animated_sprite.offset.y = ATTACK_ANIM_OFFSET
+	#if target_anim == "attack_start" or target_anim == "attack" or target_anim == "attack_end":
+		#animated_sprite.offset.y = ATTACK_ANIM_OFFSET
 		#animation_player.offset.y = ATTACK_ANIM_OFFSET
 	else:
 		animated_sprite.offset.y = 0
